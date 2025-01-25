11a1ad00694b22ae264cb25ad29387cc26bb255f
huangdi <hd20100104@163.com>
2025-01-25 22:53:56 +0800

    use caddy as a reverse proxy

--- backend/main.go
+++ backend/main.go
@@ -18,10 +18,10 @@ func main() {
 	}
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},                                       // 允许访问的来源
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},            // 允许的方法
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"}, // 允许的请求头
-		AllowCredentials: true,                                                // 是否允许携带认证信息
+		AllowOrigins:     []string{"*"},                                                    // 允许访问的来源
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},                         // 允许的方法
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "x-real-ip"}, // 允许的请求头
+		AllowCredentials: true,                                                             // 是否允许携带认证信息
 	}))
 	r.POST("/api/auth/register", middlewares.IsJsonMiddleware(), auth.Register)
 	r.POST("/api/auth/login", middlewares.IsJsonMiddleware(), auth.Login)
