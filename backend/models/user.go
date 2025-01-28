753bfbe48752cd63fe77f5b42085a0cbc381b149
huangdi <hd20100104@163.com>
2025-01-29 00:13:12 +0800

    fixed bug

--- backend/models/user.go
+++ backend/models/user.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type User struct {
-	ID                uint64 `gorm:"primaryKey"` // Snowflake ID
-	Username          string `gorm:"not null"`
-	Email             string `gorm:"unique;not null"`
-	Password          string `gorm:"not null"`
+	ID                uint64 `gorm:"primaryKey"`                 // Snowflake ID
+	Username          string `gorm:"type:varchar(255);not null"` // 指定为 VARCHAR
+	Email             string `gorm:"type:varchar(255);unique;not null"`
+	Password          string `gorm:"type:varchar(255);not null"`
 	IsVerified        bool
-	VerificationToken string
+	VerificationToken string    `gorm:"type:varchar(10)"`
 	RegisterAt        time.Time `gorm:"autoCreateTime"`
 }
