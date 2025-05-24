ac17bf21bd252292f9a1387fb4445bdeb3b769d8
Allen Sun <allen.haha@hotmail.com>
2025-05-24 13:12:17 +0800

    add owner field to course

--- ai_engine/apps/courses/models.py
+++ ai_engine/apps/courses/models.py
@@ -2,6 +2,7 @@ from tortoise import fields, models
 
 class Course(models.Model):
     id = fields.UUIDField(version=7, pk=True)
+    owner = fields.ForeignKeyField("models.User", related_name="courses")
     name = fields.CharField(max_length=255)
     description = fields.TextField()
     created_at = fields.DatetimeField(auto_now_add=True)
