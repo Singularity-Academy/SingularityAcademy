fb17d6c270146016bfb452156c0d15a177cef020
‘xhanhh’ <myxhan@outlook.com>
2025-06-28 22:52:46 +0800

    refactor: 移除router路由表

--- frontend/src/router/index.ts
+++
@@ -1,9 +0,0 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import { staticRoutes } from './routes'
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: staticRoutes
-})
-
-export default router
